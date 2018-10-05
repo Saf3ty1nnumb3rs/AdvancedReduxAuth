db7240b102d410366e7ed43c89ff2af3ba0c7c43
Josh Sample <joshuawsample@gmail.com>
2018-10-05 12:56:50 -0400

    Encrypting password when stored, token being created

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,13 @@
+const jwt = require('jwt-simple');
 const User = require("../models/user");
+const config = require('../config');
+
+
+tokenForUser = (user) => {
+    const timestamp = new Date().getTime()
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
 
 exports.signup = (req, res, next) => {
   const email = req.body.email;
@@ -27,6 +36,6 @@ exports.signup = (req, res, next) => {
         if(err) { return next(err)}
     })
     //Respond to request indicating the user was created
-    res.json({success: true})
+    res.json({token: tokenForUser(user)})
   });
 };
